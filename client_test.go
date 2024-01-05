e309d4315d52d104dc8443c441c07c7d85c934d1
WqyJh <781345688@qq.com>
2024-01-05 18:02:07 +0800

    feat: make RequestBuilder configurable

--- client_test.go
+++ client_test.go
@@ -216,7 +216,7 @@ func TestHandleErrorResp(t *testing.T) {
 func TestClientReturnsRequestBuilderErrors(t *testing.T) {
 	config := DefaultConfig(test.GetTestToken())
 	client := NewClientWithConfig(config)
-	client.requestBuilder = &failingRequestBuilder{}
+	client.config.RequestBuilder = &failingRequestBuilder{}
 	ctx := context.Background()
 
 	type TestCase struct {
@@ -409,7 +409,7 @@ func TestClientReturnsRequestBuilderErrors(t *testing.T) {
 func TestClientReturnsRequestBuilderErrorsAddtion(t *testing.T) {
 	config := DefaultConfig(test.GetTestToken())
 	client := NewClientWithConfig(config)
-	client.requestBuilder = &failingRequestBuilder{}
+	client.config.RequestBuilder = &failingRequestBuilder{}
 	ctx := context.Background()
 	_, err := client.CreateCompletion(ctx, CompletionRequest{Prompt: 1})
 	if !errors.Is(err, ErrCompletionRequestPromptTypeNotSupported) {
